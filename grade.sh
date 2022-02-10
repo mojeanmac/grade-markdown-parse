f3f7cacb208d648f2d3849abbcc99272263465e8
Molly <mojeanmac@gmail.com>
2022-02-10 12:46:56 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,3 +4,4 @@ cp GradingTests.java markdown-parse/
 cd markdown-parse
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
+echo I made an improvement
